50ab833ad20bc056017077cae11816e125daafc0
Dezsi <pdezsi96@gmail.com>
2017-11-18 10:38:19 +0100

    GridView's background is transparent+Correction: Empty tile's display

--- app/src/main/java/com/obuda/nik/picpuzzle/adapters/ImageAdapter.java
+++ app/src/main/java/com/obuda/nik/picpuzzle/adapters/ImageAdapter.java
@@ -1,6 +1,7 @@
 package com.obuda.nik.picpuzzle.adapters;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.view.View;
@@ -60,12 +61,21 @@ public class ImageAdapter extends BaseAdapter {
         }else{
             viewHolder= (ViewHolder) view.getTag();
         }
-        if(Tiles[i]!=null && Tiles[i].getPicture()!=null) {
+        if(Tiles[i].getPicture()!=null) {
             viewHolder.view.setImageBitmap(Tiles[i].getPicture());
             viewHolder.view.setImageAlpha(255);
+            viewHolder.view.setBackgroundColor(Color.parseColor("#000000")); //TODO values/styles.xml border color?
+            viewHolder.view.setPadding(1,1,1,1);
         }
-        else
+        else{
+            int j=0;
+            while (Tiles[j].getPicture()==null){
+                j++;
+            }
+            viewHolder.view.setImageBitmap(Tiles[j].getPicture());
             viewHolder.view.setImageAlpha(0);
+            viewHolder.view.setBackgroundColor(Color.argb(0,255,255,255));
+        }
         return view;
     }
 
